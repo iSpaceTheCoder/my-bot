6274fe1566c97b3af4073d37635df4e7f1ee4fb6
geenva <geenva@users.noreply.github.com>
2020-10-28 17:26:07 +0800

    ✨ hello ecapsi, welcome to your first github repo!

---
+++ index.js
@@ -0,0 +1,9 @@
+// hello! this is a comment.
+/* you can also comment like this */
+/* or maybe even... 
+like this! 
+*/
+
+// welcome. your first node.js tutorial by geneva.
+
+console.log("hi ecapsi! this is a console.log. you use it to do stuff.");
